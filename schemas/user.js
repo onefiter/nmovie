db4a874c0c368277afa9d0da4b9b126279341b1d
onefiter <onefiter@gmail.com>
2017-03-22 08:20:25 +0800

    登录注册模块

--- schemas/user.js
+++ schemas/user.js
@@ -1,14 +1,18 @@
 //1.引入mongoose模块
 var mongoose = require('mongoose')
-var MovieSchema = new mongoose.Schema({
-    director: String,
-    title: String,
-    language: String,
-    country: String,
-    summary: String,
-    flash: String,
-    poster: String,
-    year: Number,
+var bcrypt = require('bcrypt')
+var SALT_WORK_FACTOR = 10
+
+var UserSchema = new mongoose.Schema({
+    name: {
+        unique: true,
+        type: String
+    },
+    password: {
+        unique: true,
+        type: String
+    },
+
     meta: {
         createAt: {
             type: Date,
@@ -21,19 +25,30 @@ var MovieSchema = new mongoose.Schema({
     }
 })
 
-MovieSchema.pre('save', function(next) {
+UserSchem.pre('save', function(next) {
+    var user = this
     if (this.isNew) {
         this.meta.createAt = this.meta.updateAt = Date.now()
     } else {
         this.meta.updateAt = Date.now()
     }
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err)
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err)
+
+            user.password = hash
+            next()
+        })
+    })
     next()
 })
 
 /**
  * 取出数据库中所有数据
  */
-MovieSchema.statics = {
+MovieSchema.static = {
     fetch: function(cb) {
         return this
             .find({})
